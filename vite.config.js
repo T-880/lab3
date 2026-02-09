b70585d927df5fbf54340f2c0e6ff82e46b265b5
Trella <mr.trella@hotmail.com>
2026-02-09 20:14:32 +0100

    Ändrade base i config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { imagetools } from 'vite-imagetools';
 
 export default defineConfig({
-  base: './',
+  base: '/lab3/',
   plugins: [imagetools()],
   build: {
     outDir: 'docs',
